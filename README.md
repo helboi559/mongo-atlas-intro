9bd3583063d43463216b656a3ed32ec7588d6fb7
Juan Ramirez <rami8224.2007@gmail.com>
2022-05-28 15:33:55 -0400

    make a users list

--- README.md
+++ README.md
@@ -14,3 +14,70 @@
 - If page is given, the returned posts should be the next page of results given a limit
 - If sortField and sortOrder are given, the returned posts should be sorted on the field given by sortField and ordered by sortOrder. E.G. sortField="date", sortOrder="asc" should sort the result by date in ascending order.
 - If filterParam is given, the result should be filter the result based upon the filterParam field. E.G. if filterParam="category" and the filterValue="dog" the result should only show posts in which the category is dog.
+
+* Continue to build your database functionality by implementing the following functions:
+  - [OPTIONAL] Convert all the blog post id's from the string type to the number type. Hint: if you run the following code in noSqlBooster, this will convert the field type from a string to an int32 for all id's in the posts collection. Note: if your posts collection is named differently, you'll have to change the string being passed into db.getCollection() from "posts" to whatever you named your posts collection.
+    - db.getCollection("posts").update(
+      {},
+      [{
+      $set: {
+      "id":
+      {
+      $convert: {
+      input: "$id",
+      to: "int"
+      }
+      },
+      }
+      }],
+      { multi: true }
+      )
+  - findPost(blogId) should return a single blog post given an ID
+  - getPostsCollectionLength() should return a number representing the total length of the blog posts collection.
+  - makePost(blogId, title, text, author, category) should create a new blog post in mongo. Remember you need to generate and add a createdAt date, and a lastModified date to the post before inserting it into the collection. Additionally, blogId should be calculated by taking the total length of blog posts in the database and adding 1 to it. Hint: use the getPostsCollectionLength() function to quickly determine the current length of the collection.
+  - updatePost(blogId, title, text, author, category) should find a post matching the id of blogId and then update the title, text, author and category fields with the inputted information. Remember, since lastModified is a representation of when the post was last updated (including creation), you will have to update lastModified to the current date and time as well.
+  - deletePosts(blogIds) should take in an ARRAY of blogId's in the blogIds param. The function should iterate through the array of blogId's and delete all the blog posts with matching id's.
+  - Stretch Goal:
+    - Iterate through the posts collection and generate a list of author names. Create a new collection in your blogs database called users (this collection should be on the same hierarchical level as the posts collection). For every author in the list do the following:
+    - Create and insert a new user object into the users collection with the following fields:
+      - firstName
+      - lastName
+      - userId - a unique id. It can be the same scheme as the posts id, a number representing the current length of the collection + 1
+      - email - should be the following format <firstName>.<lastName>@gmail.com, all lowercase letters, with no whitespace in the email address.
+      - posts - should be a list of mongo OBJECTID's representing a list of every post that author has made.
+      - E.G. if this post exists:
+        {
+        "\_id": new ObjectId("628d233e1505f82ea360a613")
+        "createdAt": "2021-06-08T12:25:55.889Z",
+        "title": "officia",
+        "text": "Lorem Ipsum.",
+        "author": "Jacqueline Hudson",
+        "lastModified": "2022-05-24T00:55:08.105Z",
+        "category": "dolores",
+        "id": "2"
+        }
+        Then the following user should be generated and inserted into the users collection:
+        {
+        "firstName": "Jacqueline"
+        "lastName": "Hudson"
+        "userId": "1"
+        "email": "jacqueline.hudson@gmail.com"
+        "posts": [new ObjectId("628d233e1505f82ea360a613")]
+        }
+  - Super Strech Goal: Write a function getUser(email) that will lookup a user in the users collection. The function should also loop through the posts array on the user object and transform the ObjectId's into the corresponding post document before returning it. E.G. running getUser("jacqueline.hudson@gmail.com") should return:
+    - {
+      "firstName": "Jacqueline"
+      "lastName": "Hudson"
+      "userId": "1"
+      "email": "jacqueline.hudson@gmail.com"
+      "posts": [{
+      "_id": new ObjectId("628d233e1505f82ea360a613")
+      "createdAt": "2021-06-08T12:25:55.889Z",
+      "title": "officia",
+      "text": "Lorem Ipsum.",
+      "author": "Jacqueline Hudson",
+      "lastModified": "2022-05-24T00:55:08.105Z",
+      "category": "dolores",
+      "id": "2"
+      }]
+      }
--- index.js
+++ index.js
@@ -39,4 +39,138 @@ const getPosts = (limit,skip,sortField, sortOrder, filterField, filterValue)=> {
     return dbResult;
 }
 
-console.log(getPosts(10, 2, 'createdAt', 1))
+// console.log(getPosts(10, 2, 'createdAt', 1))
+//----------------------------------------------------------------------------------------
+//change the id values to numbers instead of string to existing values via changing field type
+db.getCollection("posts").update(
+    {
+        
+        //"id" : { $type: "string" }  //update all of type string
+    },
+    [{
+        $set: {
+            "id":
+            {
+                $convert: {
+                    input: "$id",
+                    to: "int", //available convert types: string|bool|int|long|double|decimal|date|timestamp|objectId ...
+                    //onError:"$id", //remain unchanged
+                    //onNull: 0, //if the input is null or missing
+                }
+            },
+        }
+    }],
+    { multi: true }
+)
+
+//return a single blog post 
+const findPost = (blogId) => {
+    
+   return db.posts.findOne({"id":blogId})
+   
+}
+// console.log(findPost(5))
+// db.posts.find({'id':5})
+//=>{
+//   "_id": "628d38a163019e865e5b2793",
+//   "createdAt": "2021-11-12T05:53:38.000Z",
+//   "title": "quod",
+//   "text": "Quam necessitatibus nihil omnis laborum neque est a a voluptates. Occaecati aliquam officiis. Sed adipisci consectetur aut nulla aperiam harum ea voluptates eos. Recusandae harum dicta voluptatem autem voluptate doloremque laudantium qui dolorum. Laudantium cupiditate excepturi fugiat ea nam. Quidem inventore pariatur esse unde laboriosam voluptate et quis fuga.\nEt dolorem culpa sint. Qui sunt dignissimos fuga et eum. Tempora error voluptas aut eum provident.\nAperiam et optio qui nesciunt dolore consequuntur dolor est. Sed eius distinctio doloribus libero atque ullam id. Laborum qui facilis magni voluptatem sed quaerat voluptatum pariatur. Autem ea eum. Sit quisquam quasi modi voluptatum rem dolores vel omnis dolorum. Porro suscipit aut id.",
+//   "author": "Sara Hickle",
+//   "lastModified": "2022-05-24T08:32:40.447Z",
+//   "category": "non",
+//   "id": 5
+// }
+
+//return a number of total length of the posts collection
+const getPostsCollectionLength= () => {
+    return db.posts.count()
+}
+// console.log(getPostsCollectionLength())
+// => 50
+
+//create post, add remaining fields (createdAt, lastModified)
+
+const makePost = (blogId,title,text,author,category) => {
+    blogId = blogId + 1
+    const now = new Date();
+    let lastModified = new Date();
+    const newPost = db.posts.insertOne({
+        'createdAt':now.toISOString(),
+        'title':title,
+        'text':text,
+        'author':author,
+        'lastModified':lastModified.toISOString(),
+        'category':category,
+        'id':blogId
+    })
+    return newPost
+}
+const postLength = getPostsCollectionLength()
+// console.log(makePost(postLength,'Among us','This is among us all. There is nothing greater.','Juan','qui'))
+
+//update post - update selected post by id - ensure last modified is updated 
+const updatePost = (blogId, title, text, author, category) => {
+    const updatePost = db.posts.updateOne(
+        {'id':blogId},
+        {
+            $currentDate: {'lastModified':true },
+            $set:{'title':title, 'text':text, 'author':author, 'category':category}
+        }
+        )
+    return updatePost
+}
+
+// console.log(updatePost(51,'updated1','this is updated', 'john s','auth'))
+
+//delete multiple posts given an array
+const deletePosts = (arr) => {
+    arr.forEach(ele => {
+        return db.posts.deleteOne({"id":ele})
+    })
+    return;
+}
+// console.log(deletePosts([50,48,49]))
+
+
+//go through all docs and generate list of all author names -- stretch goal
+    //create a new list in blogs db "users" w/ similar data 
+
+const getList = () => {
+    let authors = splitName(db.posts.distinct('author'))
+    //get arr of results with field as input via 'distinct' 
+    let posts = db.posts.distinct('_id')
+    let id = 0
+    posts.forEach((ele,index) => {
+        const ele2 = authors[index]
+        let splitted = ele2.split(' ')
+        const first = splitted[0]
+        const last = splitted[1]
+        const email = `${first}.${last}@gmail.com`
+        id = id += 1
+        db.sample.insertOne({'first':first, "last":last, 'userId':id.toString(), 'email':email, "posts":[ele]})
+    })
+    return;
+}
+
+//helper function to sort list and make names lower case **still not able to filter out "DDS" && "Mr.", playing with REGEX
+const splitName = (listNames) => {  
+    let newName = []
+    for(let j = 0 ; j < listNames.length ; j++) {
+        // console.log(words[j])
+        let words = listNames[j]
+        let newChar = ''
+        for(let i = 0 ; i < words.length ; i++) {
+            if(words[i]   )
+            if(words[i] === words[i].toUpperCase()) {
+                newChar += words[i].toLowerCase()
+            } else {
+                newChar += words[i]
+            }
+        }
+        newName.push(newChar)
+    }
+    return newName
+}
+
+getList()
